7c01deae94c6ed14b7a323590ca2e8c6b435c265
dore96 <doredelman@gmail.com>
2023-07-04 14:09:28 +0300

    Tasks logic completed need to update task.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React , {useState} from 'react';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+
+    const [tasks,setTasks] = useState(    [{
+        id: 1,
+        text: 'some text',
+        day: 'Aug 29th at 2:30pm',
+        reminder: true,
+    }])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+        <Tasks tasks = {tasks}/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import Task from "./Task";
+const Tasks = ({tasks}) => {
+    return (
+        <>
+            {tasks.map((task) => (
+                <Task key={task.id}/>
+            ))}
+        </>
+    )
+}
+
+export default Tasks

57cef42aba1fba4628b9652f9cd1167cc6d1ad08
dore96 <doredelman@gmail.com>
2023-07-04 15:53:53 +0300

    Tasks & task logic completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , {useState} from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import task from "./components/Task";
 function App() {
 
     const [tasks,setTasks] = useState(    [{
@@ -10,10 +11,26 @@ function App() {
         reminder: true,
     }])
 
+    //delete task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle reminder
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task)=>
+            task.id === id ?
+                {...task, reminder: !task.reminder} : task))
+    }
+
   return (
     <div className="container">
       <Header />
-        <Tasks tasks = {tasks}/>
+        {tasks.length > 0 ? <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+            onToggle = {toggleReminder}/>
+            : 'No Tasks to show'}
     </div>
   );
 }

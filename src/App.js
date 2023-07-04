c9c5be5c0dd3d19522ac044038b47ed7c4bf4a50
dore96 <doredelman@gmail.com>
2023-07-04 16:31:15 +0300

    Tasks & task logic

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Tasks from "./components/Tasks";
 import task from "./components/Task";
 function App() {
 
-    const [tasks,setTasks] = useState(    [{
+    const [tasks,setTasks] = useState(    [
+        {
         id: 1,
         text: 'some text',
         day: 'Aug 29th at 2:30pm',
         reminder: true,
-    }])
+        },
+        {
+            id: 2,
+            text: 'some text2',
+            day: 'Aug 30th at 2:30pm',
+            reminder: true,
+        },
+    ])
 
     //delete task
     const deleteTask = (id) => {
